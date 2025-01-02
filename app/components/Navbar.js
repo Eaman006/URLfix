07542439ea11fb3a831d7056f506130cc6e79810
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-01-02 21:55:11 +0530

    home page

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,34 +1,45 @@
-import React from 'react'
-import Link from 'next/link'
-import localFont from 'next/font/local'
-const myFont = localFont({ src: './AguDisplay-Regular-VariableFont_MORF.ttf' })
-import Image from 'next/image'
-
-
+"use client"
+import React from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'; 
+import localFont from 'next/font/local';
+const myFont = localFont({ src: './AguDisplay-Regular-VariableFont_MORF.ttf' });
+import Image from 'next/image';
 
 const Navbar = () => {
-  return (
-    <nav className='flex gap-5 px-2 py-2'>
-        <div className='mx-auto flex'>
-        <Image
-      src="/loader.gif"
-      width={30}
-      height={30}
-      alt="Picture of the author"
-    />
-            <h1 className={`text-4xl text-red-500 cursor-default ${myFont.className}`}>://URLfix</h1>
-        </div>
-        <ul className='flex gap-7 m-3'>
-            <Link href="/"> <li className='hover:text-red-600 cursor-pointer'>Home</li></Link>
-            <Link href="/about"> <li className='hover:text-red-600 cursor-pointer'>About</li></Link>
-            <Link href="/contact"> <li className='hover:text-red-600 cursor-pointer'>Contact</li></Link>
-            <li className='flex gap-5'>
-              <Link href="/generate"><button>TryNow</button></Link>
-              <Link href="https://github.com/Eaman006/URLfix.git"><button>Github</button></Link>
-            </li>
-        </ul>
-    </nav>
-  )
-}
+  const pathname = usePathname(); 
 
-export default Navbar
+  const getActiveClass = (path) => {
+    return pathname === path ? 'text-red-600' : 'hover:text-red-600';
+  };
+
+  return (
+    <nav className="flex gap-5 px-2 py-2">
+      <div className="mx-auto flex">
+        <Image src="/loader.gif" width={20} height={10} alt="Loader" />
+        <h1 className={`text-4xl text-red-500 cursor-default ${myFont.className}`}>://URLfix</h1>
+      </div>
+      <ul className="flex gap-7 m-3">
+        <li className={`cursor-pointer ${getActiveClass('/')}`}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={`cursor-pointer ${getActiveClass('/about')}`}>
+          <Link href="/about">About</Link>
+        </li>
+        <li className={`cursor-pointer ${getActiveClass('/contact')}`}>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li className="flex gap-5">
+          <Link href="/generate">
+            <button>Try Now</button>
+          </Link>
+          <Link href="https://github.com/Eaman006/URLfix.git">
+            <button>GitHub</button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Poppins } from 'next/font/google'
+import background from './background.jpg'
 
 const poppins = Poppins({
    weight:'800',
@@ -7,8 +8,19 @@ const poppins = Poppins({
 
 export default function Home() {
   return (
-    <div className="m-2">
-      <div className={`text-center font-bold text-2xl ${poppins.className}`}>
+    <div className="relative md:w-full w-[150%] h-[130vh] md:h-[91vh]">
+       <Image
+    alt="background"
+    src={background}
+    placeholder="blur"
+    quality={100}
+    fill
+    sizes="100vw"
+    style={{
+      objectFit: 'cover',
+    }}
+  />
+      <div className={`absolute inset-0 flex items-center justify-center text-center font-bold text-2xl text-white ${poppins.className}`}>
         Shortening url is much easier now
       </div>
     </div>
