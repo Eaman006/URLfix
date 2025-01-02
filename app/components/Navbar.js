ef88bdff15d03f43792ddfa460379847479547f9
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-01-02 16:28:26 +0530

    loader added

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,13 +2,20 @@ import React from 'react'
 import Link from 'next/link'
 import localFont from 'next/font/local'
 const myFont = localFont({ src: './AguDisplay-Regular-VariableFont_MORF.ttf' })
+import Image from 'next/image'
 
 
 
 const Navbar = () => {
   return (
     <nav className='flex gap-5 px-2 py-2'>
-        <div className='mx-auto'>
+        <div className='mx-auto flex'>
+        <Image
+      src="/loader.gif"
+      width={30}
+      height={30}
+      alt="Picture of the author"
+    />
             <h1 className={`text-4xl text-red-500 cursor-default ${myFont.className}`}>://URLfix</h1>
         </div>
         <ul className='flex gap-7 m-3'>
