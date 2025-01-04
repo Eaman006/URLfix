3c7c34ea73fd26bff75d4af2eede705ccc9d3500
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-01-04 17:00:24 +0530

    generate page added

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+
 
 const page = () => {
+  const [url, seturl] = useState("")
+  const [shorturl, setshorturl] = useState("")
+  const [generated, setgenerated] = useState(false)
+  
   return (
     <div>
-      this is generate page
+      <div className=' mx-auto max-w-lg'>
+      <div>Generate your Short URLS</div>
+      <div className='flex flex-col text-black gap-5'>
+      <input className='p-4 focus:outline-purple-600' type="text" placeholder='Enter your url here' onChange={(e)=>seturl(e.target.value)} value={url} />
+      <input className='p-4 focus:outline-purple-600' type="text" placeholder='Enter your preffered short url text here' onChange={(e)=>setshorturl(e.target.value)} value={shorturl} />
+      </div>
+      <button>Generate</button>
+      </div>
     </div>
   )
 }
