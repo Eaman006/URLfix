d95ea56d015fa4b8f708904d2b3ed127ae7ff8a5
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-01-08 20:17:33 +0530

    url shortner fuctionality added

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,11 +1,40 @@
 "use client"
 import React, { useState } from 'react'
+import Link from 'next/link'
 
 
 const page = () => {
   const [url, seturl] = useState("")
   const [shorturl, setshorturl] = useState("")
   const [generated, setgenerated] = useState(false)
+  const generate = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "url": url,
+      "shorturl": shorturl
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+
+    fetch("/api/generate", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setgenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
+        seturl("")
+        setshorturl("")
+        console.log(result)
+        alert("result generated successfully")
+      })
+      .catch((error) => console.error(error));
+  }
+
 
   return (
     <div className='pt-[10%]'>
@@ -15,9 +44,14 @@ const page = () => {
           <input className='bg-[#ffffff28] p-4 focus:outline-purple-600 rounded-full' type="text" placeholder='Enter your url here' onChange={(e) => seturl(e.target.value)} value={url} />
           <input className='bg-[#fcfbfb28] p-4 focus:outline-purple-600 rounded-full' type="text" placeholder='Enter your preffered short url text here' onChange={(e) => setshorturl(e.target.value)} value={shorturl} />
         </div>
-        <div className='flex justify-center items-center my-5'>
-        <button className='bg-purple-400 hover:bg-purple-600 hover:active:bg-purple-900 py-2 px-5 rounded-full font-bold'>Generate</button>
+        <div onClick={generate} className='flex justify-center items-center my-5'>
+          <button className='bg-purple-400 hover:bg-purple-600 hover:active:bg-purple-900 py-2 px-5 rounded-full font-bold'>Generate</button>
         </div>
+        {generated && <code>
+          <Link target='_blank' href={generated}>{generated}</Link>
+        </code>
+        }
+        
       </div>
     </div>
   )
