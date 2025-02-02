e14bfa0bfe6b6fea00a376d140948abceaead935
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-02-02 23:48:03 +0530

    home page link added

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,13 +1,13 @@
 "use client"
 import React from 'react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation'; 
+import { usePathname } from 'next/navigation';
 import localFont from 'next/font/local';
 const myFont = localFont({ src: './AguDisplay-Regular-VariableFont_MORF.ttf' });
 import Image from 'next/image';
 
 const Navbar = () => {
-  const pathname = usePathname(); 
+  const pathname = usePathname();
 
   const getActiveClass = (path) => {
     return pathname === path ? 'text-red-600' : 'hover:text-red-600';
@@ -16,8 +16,10 @@ const Navbar = () => {
   return (
     <nav className="flex gap-5 px-2 py-2">
       <div className="mx-auto flex">
-        <Image src="/loader.gif" width={20} height={10} alt="Loader" />
-        <h1 className={`text-4xl text-red-500 cursor-default ${myFont.className}`}>://URLfix</h1>
+        <Link href="/" className='flex'>
+          <Image src="/loader.gif" width={20} height={10} alt="Loader" />
+          <h1 className={`text-4xl text-red-500 cursor-default ${myFont.className} cursor-pointer`}>://URLfix</h1>
+        </Link>
       </div>
       <ul className="flex gap-7 m-3">
         <li className={`cursor-pointer ${getActiveClass('/')}`}>
@@ -29,7 +31,7 @@ const Navbar = () => {
         <li className={`cursor-pointer ${getActiveClass('/contact')}`}>
           <Link href="/contact">Contact</Link>
         </li>
-        
+
       </ul>
     </nav>
   );
